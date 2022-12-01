0ba6cdd7cbf517a8d15036e0203cb1b19545ca23
eineinhornmiau <8723673+eineinhornmiau@users.noreply.github.com>
2022-12-01 16:14:20 +0100

    :tada: Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,50 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var highest = 0
+        var temp = 0
+
+        for (line in input) {
+            if (line == "") {
+                if (temp > highest) {
+                    highest = temp
+                }
+                temp = 0
+                continue
+            }
+
+            temp += line.toInt()
+        }
+
+        return highest
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves: MutableList<Int> = mutableListOf()
+        var temp = 0
+
+        for (i in input.indices) {
+            if (input[i] != "") {
+                temp += input[i].toInt()
+            }
+
+            if (input[i] == "" || i == input.indices.last) {
+                elves.add(temp)
+                temp = 0
+                continue
+            }
+        }
+
+        elves.sortDescending()
+
+        println(elves)
+
+        return elves[0] + elves[1] + elves[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
